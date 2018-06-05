330fc2f08aa3de7faa9c8a9382c6530299801d0e
LilyYan2001 <lyan@stuy.edu>
2018-06-05 14:23:10 -0400

    callcenter

--- CallCenter.java
+++ CallCenter.java
@@ -7,9 +7,26 @@ import java.util.LinkedList;
 public class CallCenter {
 
     //instance vars
-    private PriorityQueue<Crime> crimes;
+    private PriorityQueue crimes;
     private ALHeapMin inventory;
     private LinkedList<Busters> busters;
+    private int time;
+    
+
+    long startTime = System.currentTimeMillis();
+    long endTime = startTime + timePassed;
+
+    public CallCenter(int inputTime) {
+	time = inputTime;
+	crimes = new PriorityQueue();
+    }
+    
+
+    public void chooseBusters(Crime input) {
+    }
+
+    public void chooseEquipment(Crime input, int root) {
+    }
 
     
     public static void main(String[] args) {
